4424eb016a5e50bf62b5d5e9a4716489215209ed
Anish Kumar <gamingvsgame002@gmail.com>
2024-04-19 18:51:22 -0400

    feat: added dishes and menu tables

---
+++ Group_Project/api/models/menu.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+
+class Menu(Base):
+   __tablename__ = "menu"
+
+
+   id = Column(Integer, autoincrement=True)
+   category = Column(String(100), unique=True, nullable=True)
+
+
+   dishes = relationship("dishes", back_populates="menu")
+
+
+git
