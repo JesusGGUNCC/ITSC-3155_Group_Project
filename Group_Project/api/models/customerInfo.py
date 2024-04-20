a9aefdc902edaccdeb132fc8607a6c7b3f18045b
JesusGG03 <jgeronimo2704@gmail.com>
2024-04-19 20:35:29 -0400

    feat: Readjusted the order_details.py to fit our table

--- Group_Project/api/models/customerInfo.py
+++ Group_Project/api/models/customerInfo.py
@@ -7,9 +7,9 @@ class Customer(Base):
     __tablename__ = "customers"
 
     id = Column(Integer, autoincrement=True)
-    name = Column(String(100), index=True, unique=True)
-    email = Column(String(100), index=True, primary_key=True, unique=True)
-    phone = Column(String(15), index=True, unique=True)
-    address = Column(String(100), index=True, unique=True)
+    name = Column(String(100), index=True, primary_key=True)
+    email = Column(String(100), index=True, primary_key=True)
+    phone = Column(String(15), index=True, primary_key=True)
+    address = Column(String(100), index=True, primary_key=True)
 
     accounts = relationship("Account", back_populates="customers")
--- Group_Project/api/models/order_details.py
+++ Group_Project/api/models/order_details.py
@@ -7,9 +7,17 @@ class OrderDetail(Base):
     __tablename__ = "order_details"
 
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    order_id = Column(Integer, ForeignKey("orders.id"))
-    sandwich_id = Column(Integer, ForeignKey("sandwiches.id"))
-    amount = Column(Integer, index=True, nullable=False)
 
-    sandwich = relationship("Sandwich", back_populates="order_details")
-    order = relationship("Order", back_populates="order_details")
+    cust_name = Column(String(100), ForeignKey("customers.name"))
+    cust_email = Column(String(100), ForeignKey("customers.email"))
+    cust_phone = Column(String(15), ForeignKey("customers.phone"))
+    cust_address = Column(String(100), ForeignKey("customers.address"))
+
+    dish_id = Column(Integer, ForeignKey("dishes.id"))
+
+
+    name = relationship("Customer", back_populates="order_details")
+    email = relationship("Customer", back_populates="order_details")
+    phone = relationship("Customer", back_populates="order_details")
+    address = relationship("Customer", back_populates="order_details")
+    dish = relationship("Dish", back_populates="order_details")
