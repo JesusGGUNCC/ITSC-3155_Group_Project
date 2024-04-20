763c4e0aea6e84c4207ea95e8f242260e55e7e8e
JesusGG03 <jgeronimo2704@gmail.com>
2024-04-19 22:18:55 -0400

    feat: Added rating.py in Schema

---
+++ Group_Project/api/schemas/ratings.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+from .dishes import Dish
+
+class RatingBase(BaseModel):
+    score: float
+    review: str
+
+class RatingCreate(RatingBase):
+    pass
+
+class RatingUpdate(BaseModel):
+    score: Optional[float] = None
+    review: Optional[str] = None
+
+class Rating(RatingBase):
+    id: int
+    dish: Dish = None
+
+    class ConfigDict:
+        from_attribute = None
+
