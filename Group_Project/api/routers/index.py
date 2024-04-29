04c911455a629f4749222c7eae00e761876bf23c
Levi <ljohn245@uncc.edu>
2024-04-29 14:08:20 -0400

    feat: updated 'index.py' with promotions

--- Group_Project/api/routers/index.py
+++ Group_Project/api/routers/index.py
@@ -1,8 +1,9 @@
-from . import orders, order_details, dishes
+from . import orders, order_details, dishes, promotions
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(dishes.router)
+    app.include_router(promotions.router)
 
