8470c43704a6a5dc316b352526dff1104cf0c6b2
JesusGG03 <jgeronimo2704@gmail.com>
2024-05-01 16:12:41 -0400

    Refactor: corrected account table

--- Group_Project/api/models/account.py
+++ Group_Project/api/models/account.py
@@ -9,8 +9,6 @@ class Account(Base):
     id = Column(Integer, autoincrement=True)
     username = Column(String(100), ForeignKey("customers.email"), primary_key=True, index=True)
     password = Column(String(100), index=True)
-    name = Column(String(100), ForeignKey("customers.name"))
-    address = Column(String(100), ForeignKey("customers.address"))
-    phone = Column(String(100), ForeignKey("customers.phone"))
+
 
     customers = relationship("Customer", back_populates="accounts")
