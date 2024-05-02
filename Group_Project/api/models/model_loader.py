06b48b3d20e35737b1207adbaef20f9e28fcf08f
JesusGG03 <jgeronimo2704@gmail.com>
2024-05-02 18:09:16 -0400

    refactor: fixed model loader

--- Group_Project/api/models/model_loader.py
+++ Group_Project/api/models/model_loader.py
@@ -1,12 +1,10 @@
-from . import recipes, sandwiches, resources, account, customerInfo, dishes, menu, rating, orders, order_details
+from . import account, customerInfo, dishes, menu, rating, orders, order_details
 
 from ..dependencies.database import engine
 
 
 def index():
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
+
     customerInfo.Base.metadata.create_all(engine)
     account.Base.metadata.create_all(engine)
     dishes.Base.metadata.create_all(engine)
