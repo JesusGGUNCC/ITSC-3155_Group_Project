a2ef638fbbde8560dfa043bf678374cd2b227d63
JesusGG03 <jgeronimo2704@gmail.com>
2024-05-02 22:18:48 -0400

    Refactor: fixed index.py

--- Group_Project/api/routers/index.py
+++ Group_Project/api/routers/index.py
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-from . import dishes, customers, accounts, menu, orders, ratings
-=======
-<<<<<<< HEAD
-from . import dishes, customers, accounts, menu, orders, promotions
-=======
-from . import dishes, customers, accounts, menu, orders, order_details
->>>>>>> 428cbce5e64e5da05909c9aa37606521da09f9cf
->>>>>>> 8f9872814a9b5352ba60f4d1edbf16344166aee4
+from . import dishes, customers, accounts, menu, orders, promotions, ratings, payments
+
 
 
 
@@ -17,12 +10,6 @@ def load_routes(app):
     app.include_router(customers.router)
     app.include_router(accounts.router)
     app.include_router(orders.router)
-<<<<<<< HEAD
     app.include_router(ratings.router)
-=======
-<<<<<<< HEAD
     app.include_router(promotions.router)
-=======
-    app.include_router(order_details.router)
->>>>>>> 428cbce5e64e5da05909c9aa37606521da09f9cf
->>>>>>> 8f9872814a9b5352ba60f4d1edbf16344166aee4
+    app.include_router(payments.router)
